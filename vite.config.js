c20b962a05a029c9bfcc319c3762dcfab5156de9
OhMinSeop <mins5190@gmail.com>
2021-12-03 00:39:40 +0900

    MOD: 모듈 버전 변경

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import reactJsx from 'vite-react-jsx'
+import path from 'path'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [reactJsx(), reactRefresh()],
 })
